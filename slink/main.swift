dc3703bdba3e13bab78594f69907a5c52800c91a
Christopher Hannah <christopher.hannah@me.com>
2017-03-23 17:08:56 +0000

    Initial Commit

---
+++ slink/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  slink
+//
+//  Created by Christopher Hannah on 23/03/2017.
+//  Copyright © 2017 Christopher Hannah. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
